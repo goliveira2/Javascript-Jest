cd24517edef849e80bbd2368ee6f4c68537a35a5
Gabriel Leal <gabriel.sala.oliveira@gmail.com>
2020-03-23 17:35:01 -0700

    Fizzbuzz

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,22 @@
+const n = 5;
+
+const myFizzbuzzFunc = {
+    fizzbuzz(n){
+        for(let i = 1; i <= n; i++){
+            if(i % 3 === 0 && i % 5 === 0){
+                console.log('fizzbuzz');
+            } else if (i % 3 === 0){
+                console.log('fizz');
+            } else if (i % 5 === 0){
+                console.log('buzz');
+            } else{
+                console.log(i)
+            }
+        }
+    }
+}
+
+
+// console.log(myFizzbuzzFunc.fizzbuzz(n))
+
+module.exports(myFizzbuzzFunc.fizzbuzz(n))
