fa68f20b5347dae63a16b87aa4684eb229a0cc6a
goliveira2 <gabriel.sala.oliveira@gmail.com>
2020-04-05 13:32:16 -0700

    Matrix test

--- matrix-espiral/matrixespiral.js
+++ matrix-espiral/matrixespiral.js
@@ -1,42 +1,46 @@
-const n = 3;
-
-const myMatrixFunc = {
-    matrix(n){
-        const results = []
-        for( let i = 0 ; i < n ; i++){
-            results.push([])
-        }
-        let counter  = 1;
-        let startColumn = n -1;
-        let endColumn = 0;
-        let startRow = 0;
-        let endRow = n - 1;
-        while (startColumn <= endColumn && startRow <= endRow){
-            for(let i =  startColumn; i <= endColumn ; i++){
-                results[startRow][i] = counter;
-                counter++
-            }
-            startRow++;
-
-            for (let i = startRow; i <=  endRow; i++){
-                results[i][endColumn] = counter;
-                counter++;
-            }
-            endColumn--;
-            
-            for(let i = endColumn; i >= startRow; i--){
-                results[endRow][i] = counter;
-                counter++;
-            }
-            endRow--;
-            for(let i =  endRow; i >= startRow; i--){
-                results[i][startColumn] = counter;
-                counter++;
-            }
-            startColumn++;
-        }
-        return results;
+function matrix(n) {
+    const results = [];
+  
+    for (let i = 0; i < n; i++) {
+      results.push([]);
     }
-}
-
-module.exports = myMatrixFunc.matrix
+  
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+    while (startColumn <= endColumn && startRow <= endRow) {
+      // Top row
+      for (let i = startColumn; i <= endColumn; i++) {
+        results[startRow][i] = counter;
+        counter++;
+      }
+      startRow++;
+  
+      // Right column
+      for (let i = startRow; i <= endRow; i++) {
+        results[i][endColumn] = counter;
+        counter++;
+      }
+      endColumn--;
+  
+      // Bottom row
+      for (let i = endColumn; i >= startColumn; i--) {
+        results[endRow][i] = counter;
+        counter++;
+      }
+      endRow--;
+  
+      // start column
+      for (let i = endRow; i >= startRow; i--) {
+        results[i][startColumn] = counter;
+        counter++;
+      }
+      startColumn++;
+    }
+  
+    return results;
+  }
+  
+  module.exports = matrix;
